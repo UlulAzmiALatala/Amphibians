996f93b3315953051a60d532841bdf5cb0d44737
Ulul Azmi A. Latala <133023139+UlulAzmiALatala@users.noreply.github.com>
2024-12-12 22:33:14 +0700

    Pertemuan 12

--- app/src/main/java/ulul/azmi/a/latala/amphibians/network/AmphibiansApiService.kt
+++ app/src/main/java/ulul/azmi/a/latala/amphibians/network/AmphibiansApiService.kt
@@ -1,9 +1,20 @@
-package ulul.azmi.a.latala.amphibians.network
+package ulul.azmi.a.latala.amphibians.network // Paket untuk pengelolaan API di aplikasi
 
-import ulul.azmi.a.latala.amphibians.model.Amphibian
-import retrofit2.http.GET
+// Import model data dan anotasi yang diperlukan
+import ulul.azmi.a.latala.amphibians.model.Amphibian // Model data untuk menyimpan informasi tentang amphibian
+import retrofit2.http.GET // Anotasi Retrofit untuk permintaan HTTP GET
 
+/**
+ * Interface untuk mendefinisikan layanan API Amphibians.
+ * Digunakan oleh Retrofit untuk mengelola panggilan jaringan.
+ */
 interface AmphibiansApiService {
-    @GET("amphibians")
-    suspend fun getAmphibians(): List<Amphibian>
+    /**
+     * Fungsi untuk mendapatkan daftar amphibian dari endpoint "amphibians".
+     * 
+     * @return List<Amphibian>: Daftar objek Amphibian yang diterima dari API.
+     * suspend: Fungsi berjalan secara asynchronous untuk mendukung coroutine.
+     */
+    @GET("amphibians") // Anotasi untuk menetapkan endpoint HTTP GET
+    suspend fun getAmphibians(): List<Amphibian> // Mengembalikan daftar Amphibian
 }
