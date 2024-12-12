a7dca06a6e57348f094d42da42dc4a13da64072c
Ulul Azmi A. Latala <133023139+UlulAzmiALatala@users.noreply.github.com>
2024-12-12 22:30:58 +0700

    Pertemuan 12

--- app/src/main/java/ulul/azmi/a/latala/amphibians/model/Amphibian.kt
+++ app/src/main/java/ulul/azmi/a/latala/amphibians/model/Amphibian.kt
@@ -1,15 +1,20 @@
-package ulul.azmi.a.latala.amphibians.model
+package ulul.azmi.a.latala.amphibians.model // Paket untuk model data aplikasi
 
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
+// Import library yang diperlukan untuk serialisasi data
+import kotlinx.serialization.SerialName // Digunakan untuk memberi nama khusus pada properti JSON
+import kotlinx.serialization.Serializable // Menandakan kelas dapat diserialisasi
 
 /**
- * Data class that defines an amphibian which includes a name, type, description, and image URL.
+ * Data class yang mendefinisikan struktur data untuk amphibian, termasuk:
+ * - Nama (name)
+ * - Jenis (type)
+ * - Deskripsi (description)
+ * - URL gambar (imgSrc)
  */
-@Serializable
+@Serializable // Menandakan bahwa kelas ini dapat digunakan untuk serialisasi/deserialisasi JSON
 data class Amphibian(
-    val name: String,
-    val type: String,
-    val description: String,
-    @SerialName("img_src") val imgSrc: String
+    val name: String, // Nama dari amphibian
+    val type: String, // Jenis dari amphibian (misalnya katak, salamander)
+    val description: String, // Deskripsi mengenai amphibian
+    @SerialName("img_src") val imgSrc: String // URL gambar, dipetakan ke "img_src" dari JSON
 )
