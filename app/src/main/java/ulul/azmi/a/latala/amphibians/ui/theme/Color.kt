0cf627a6bd0d875699425648b68ea7a1c006e7e1
Ulul Azmi A. Latala <133023139+UlulAzmiALatala@users.noreply.github.com>
2024-12-12 22:56:51 +0700

    Pertemuan 12

--- app/src/main/java/ulul/azmi/a/latala/amphibians/ui/theme/Color.kt
+++ app/src/main/java/ulul/azmi/a/latala/amphibians/ui/theme/Color.kt
@@ -1,11 +1,13 @@
-package ulul.azmi.a.latala.amphibians.ui.theme
+package ulul.azmi.a.latala.amphibians.ui.theme // Tema aplikasi
 
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color // Warna yang digunakan dalam tema
 
-val md_theme_light_background = Color(0xFFFCFDF6)
-val md_theme_light_surface = Color(0xFFFCFDF6)
-val md_theme_light_surfaceVariant = Color(0xFFDEE5D8)
+// Warna untuk tema terang
+val md_theme_light_background = Color(0xFFFCFDF6) // Warna latar belakang untuk tema terang
+val md_theme_light_surface = Color(0xFFFCFDF6) // Warna permukaan untuk tema terang
+val md_theme_light_surfaceVariant = Color(0xFFDEE5D8) // Warna varian permukaan untuk tema terang
 
-val md_theme_dark_background = Color(0xFF1A1C19)
-val md_theme_dark_surface = Color(0xFF1A1C19)
-val md_theme_dark_surfaceVariant = Color(0xFF424940)
+// Warna untuk tema gelap
+val md_theme_dark_background = Color(0xFF1A1C19) // Warna latar belakang untuk tema gelap
+val md_theme_dark_surface = Color(0xFF1A1C19) // Warna permukaan untuk tema gelap
+val md_theme_dark_surfaceVariant = Color(0xFF424940) // Warna varian permukaan untuk tema gelap
