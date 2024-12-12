5044585e5cd6163ecc7926a1e15eb72702491b37
Ulul Azmi A. Latala <133023139+UlulAzmiALatala@users.noreply.github.com>
2024-12-12 22:58:46 +0700

    Pertemuan 12

--- app/src/main/java/ulul/azmi/a/latala/amphibians/ui/theme/Type.kt
+++ app/src/main/java/ulul/azmi/a/latala/amphibians/ui/theme/Type.kt
@@ -9,10 +9,10 @@ import androidx.compose.ui.unit.sp
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = FontFamily.Default, // Font default yang digunakan
+        fontWeight = FontWeight.Normal, // Berat font normal
+        fontSize = 16.sp, // Ukuran font 16 spasi
+        lineHeight = 24.sp, // Tinggi baris 24 spasi
+        letterSpacing = 0.5.sp // Spasi antar huruf 0.5 spasi
     )
 )
